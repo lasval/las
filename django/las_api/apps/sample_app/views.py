b97b811d6e3b080eec37626390790604f5860e06
las <las@gymtinc.com>
2022-08-18 16:00:06 +0900

    test

--- django/las_api/apps/sample_app/views.py
+++ django/las_api/apps/sample_app/views.py
@@ -10,7 +10,7 @@ from rest_framework.response import Response
 def sample_app_view(request):
     """
     get:
-        - sample_app_view
+        - sample_app_view_ㅁㅁㅁㅁㅁ
     """
 
     result = {"result": "HelloWorld!!"}
