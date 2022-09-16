260e67a718a3cab39cacc0f0ff03967b965ef310
las <las@gymtinc.com>
2022-09-16 15:40:10 +0900

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
  
 # Blue 를 기준으로 현재 떠있는 컨테이너를 체크한다.
-EXIST_BLUE=$(docker-compose -p docker_web1-blue -f docker-compose.blue.yaml ps | grep Up)
+EXIST_BLUE=$(docker-compose -p docker -f docker-compose-blue.yml ps | grep Up | grep las-django-blue1)
  
 # 컨테이너 스위칭
 if [ -z "$EXIST_BLUE" ]; then
     echo "blue up"
-    docker-compose -p docker_web1-blue -f docker-compose.blue.yaml up -d
+    docker-compose -p docker -f docker-compose-blue.yml up -d
     BEFORE_COMPOSE_COLOR="green"
     AFTER_COMPOSE_COLOR="blue"
 else
     echo "green up"
-    docker-compose -p docker_web1-green -f docker-compose.green.yaml up -d
+    docker-compose -p docker -f docker-compose-green.yml up -d
     BEFORE_COMPOSE_COLOR="blue"
     AFTER_COMPOSE_COLOR="green"
 fi
@@ -19,9 +19,9 @@ fi
 sleep 10
  
 # 새로운 컨테이너가 제대로 떴는지 확인
-EXIST_AFTER=$(docker-compose -p docker_web1-green -f docker-compose.green.yaml ps | grep Up)
+EXIST_AFTER=$(docker-compose -p docker -f docker-compose-${AFTER_COMPOSE_COLOR}.yml ps | grep Up | grep las-django-${AFTER_COMPOSE_COLOR}1)
 if [ -n "$EXIST_AFTER" ]; then
     # 이전 컨테이너 종료
-    docker-compose -p docker_web1-blue -f docker-compose.blue.yaml down
-    echo "blue down"
+    docker-compose -f docker-compose-${BEFORE_COMPOSE_COLOR}.yml down
+    echo "$BEFORE_COMPOSE_COLOR down"
 fi
