b31a6a51112a658efb252d836b394409e67807b0
las <las@gymtinc.com>
2022-09-19 09:22:34 +0900

    fix

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,10 @@ sleep 10
 # 새로운 컨테이너가 제대로 떴는지 확인
 EXIST_AFTER=$(docker-compose -p docker -f docker-compose-${AFTER_COMPOSE_COLOR}.yml ps | grep Up | grep las-django-${AFTER_COMPOSE_COLOR}1)
 if [ -n "$EXIST_AFTER" ]; then
+    # conf.d 수정후 리로드 
+    docker exec -ti las-nginx-${AFTER_COMPOSE_COLOR} sh -c "cp /etc/nginx/nginx-${AFTER_COMPOSE_COLOR}.conf /etc/nginx/conf.d/nginx.conf"
+    docker exec -it las-nginx-${AFTER_COMPOSE_COLOR} nginx -s reload
+    echo "nginx reload"
     # 이전 컨테이너 종료
     docker-compose -f docker-compose-${BEFORE_COMPOSE_COLOR}.yml down
     echo "$BEFORE_COMPOSE_COLOR down"
